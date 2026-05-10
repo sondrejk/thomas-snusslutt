952f49e9e4d9ecb24b31f9592e270559a87015eb
sondrejk <sondrejk@gmail.com>
2026-05-10 21:26:59 +0200

    add: change title

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>DETTE KLARER DU THOMAS!</h1>
+      <h1>DETTE KLARER DU THOMAAAAAAAS!</h1>
       <h3>(Ikke ment for utenforstående)</h3>
       <div id="memes">
-        <img src="https://i.imgflip.com/a86lfm.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a86lp9.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a86m0j.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a86ma5.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a86mlf.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kvgv.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kvnm.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kvq8.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kvwd.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kw3t.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kw85.jpg" title="made at imgflip.com"/>
-        <img src="https://i.imgflip.com/a8kwjh.jpg" title="made at imgflip.com"/>
-
+        <img
+          src="https://i.imgflip.com/a86lfm.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a86lp9.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a86m0j.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a86ma5.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a86mlf.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kvgv.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kvnm.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kvq8.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kvwd.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kw3t.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kw85.jpg"
+          title="made at imgflip.com"
+        />
+        <img
+          src="https://i.imgflip.com/a8kwjh.jpg"
+          title="made at imgflip.com"
+        />
       </div>
-
     </>
-  )
+  );
 }
-export default App
+export default App;
